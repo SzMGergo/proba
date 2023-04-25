4e1430d16d9c7a5c69c9fa00e41bf5899d24bfff
Gergo53 <sz.m.gergo53@gmail.com>
2023-04-25 11:02:11 +0200

    ml lol

--- util.py
+++ util.py
@@ -10,10 +10,10 @@ def szorzas(a, b):
 def osztas(a,b):
     return a/b
 
-def generate_synthetic.data(x, coefficients, seed=42, noise_std=1):
+def generate_synthetic_data(x, coefficients, seed=42, noise_std=1):
     np.random.seed(seed)
-    y = np.polyval(coefficients[::-1], x) + np.random.normal(0, noise:std, len(x))
-return x, y
+    y = np.polyval(coefficients[::-1], x) + np.random.normal(0, noise_std, len(x))
+    return x, y
 
 def visualize_data(x, y):
     plt.scatter(x, y)
@@ -24,6 +24,8 @@ def visualize_data(x, y):
 
 def main():
     coefficients = [1, 0.02, -0.002, 0.014]
-    x_values = np.linspace[-10, 10, 100]
+    x_values = np.linspace(-10, 10, 100)
     x, y = generate_synthetic_data(x_values, coefficients)
     visualize_data(x, y)
+
+main()
